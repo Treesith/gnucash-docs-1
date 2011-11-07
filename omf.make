Treesith/gnucash-docs-1

commit e126e75a1d5368bbf3d0f981170c9003c8655c2b

Bug #661705 : Fix omf folders path in order to be picked up by yelp. Patch provided by Sébastien Villemot.
git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash-docs/trunk@21467 57a11ea4-9604-0410-9ed3-97b8803252fd

omf.make
@@ -26,7 +26,7 @@
 # 	Version: 0.1.2 (last updated: March 20, 2002)
 #
 
-omf_dest_dir=$(datadir)/omf/gnucash-docs
+omf_dest_dir=$(datadir)/omf
 scrollkeeper_localstate_dir = $(localstatedir)/scrollkeeper
 
 omf: omf_timestamp
@@ -40,17 +40,21 @@ omf_timestamp: $(omffile)
 install-data-hook-omf:
 	$(mkinstalldirs) "$(DESTDIR)$(omf_dest_dir)"
 	for file in $(omffile); do \
-	$(INSTALL_DATA) "$$file.out" "$(DESTDIR)$(omf_dest_dir)/$$file"; \
-	done
-	@if test "x$(_ENABLE_SK)" = "xtrue"; then \
-	scrollkeeper-update -p "$(scrollkeeper_localstate_dir)" -o "$(DESTDIR)$(omf_dest_dir)"; \
-	fi;
+  omfdir="$(DESTDIR)$(omf_dest_dir)/`echo $$file | sed 's/-[^-]*\.omf$$//'`"; \
+	$(mkinstalldirs) "$$omfdir"; \
+	$(INSTALL_DATA) "$$file.out" "$$omfdir/$$file"; \
+	if test "x$(_ENABLE_SK)" = "xtrue"; then \
+	scrollkeeper-update -p "$(scrollkeeper_localstate_dir)" -o "$$omfdir"; \
+	fi; \
+  done;
 
 uninstall-local-omf:
-	@if test "x$(_ENABLE_SK)" == "xtrue"; then \
-	scrollkeeper-uninstall -p "$(scrollkeeper_localstate_dir)" "$(DESTDIR)$(omf_dest_dir)/$$file"; \
+	for file in $(omffile); do \
+  omfdir="$(DESTDIR)$(omf_dest_dir)/`echo $$file | sed 's/-[^-]*\.omf$$//'`"; \
+	if test "x$(_ENABLE_SK)" == "xtrue"; then \
+	scrollkeeper-uninstall -p "$(scrollkeeper_localstate_dir)" "$$omfdir/$$file"; \
 	fi; \
-	for file in $(srcdir)/*.omf; do \
-	  basefile=`echo $$file | sed -e  's,^.*/,,'`; \
-	  rm -f "$(DESTDIR)$(omf_dest_dir)/$$basefile"; \
-	done
+	rm -f "$$omfdir/$$file; \
+  rmdir "$$omfdir"; \
+  done;
+

commit 7d8616c59385765b0cbf3d7f902e5fcf9cdbf9ab

Fix uninstall error introduced by r21467
git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash-docs/trunk@21531 57a11ea4-9604-0410-9ed3-97b8803252fd

omf.make
@@ -40,7 +40,7 @@ omf_timestamp: $(omffile)
 install-data-hook-omf:
 	$(mkinstalldirs) "$(DESTDIR)$(omf_dest_dir)"
 	for file in $(omffile); do \
-  omfdir="$(DESTDIR)$(omf_dest_dir)/`echo $$file | sed 's/-[^-]*\.omf$$//'`"; \
+	omfdir="$(DESTDIR)$(omf_dest_dir)/`echo $$file | sed 's/-[^-]*\.omf$$//'`"; \
 	$(mkinstalldirs) "$$omfdir"; \
 	$(INSTALL_DATA) "$$file.out" "$$omfdir/$$file"; \
 	if test "x$(_ENABLE_SK)" = "xtrue"; then \
@@ -50,11 +50,11 @@ install-data-hook-omf:
 
 uninstall-local-omf:
 	for file in $(omffile); do \
-  omfdir="$(DESTDIR)$(omf_dest_dir)/`echo $$file | sed 's/-[^-]*\.omf$$//'`"; \
+	omfdir="$(DESTDIR)$(omf_dest_dir)/`echo $$file | sed 's/-[^-]*\.omf$$//'`"; \
 	if test "x$(_ENABLE_SK)" == "xtrue"; then \
 	scrollkeeper-uninstall -p "$(scrollkeeper_localstate_dir)" "$$omfdir/$$file"; \
 	fi; \
 	rm -f "$$omfdir/$$file"; \
-  rmdir "$$omfdir"; \
+	rmdir --ignore-fail-on-non-empty "$$omfdir"; \
   done;
 